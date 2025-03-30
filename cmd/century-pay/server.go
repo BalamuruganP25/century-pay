b3af3b65b95c8d5442c8af41ae4a0b57dad3ca93
BalamuruganP25 <balapandiyarajanp25@gmail.com>
2025-03-30 22:17:52 +0530

    add transaction history

--- cmd/century-pay/server.go
+++ cmd/century-pay/server.go
@@ -13,7 +13,7 @@ func initWebServer() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("v1/transaction/transfer_money", handler.TransferMoney(bank))
-	r.Get("v1/balance/{user}", handler.GetUserBalance())
-	r.Get("v1/transaction/history/{user}", handler.GetTransacationHistory())
+	r.Get("v1/transaction/{user}/balance", handler.GetUserBalance())
+	r.Get("v1/transaction/{user}/transaction_history", handler.GetTransacationHistory(bank))
 	http.ListenAndServe(":8080", r)
 }
